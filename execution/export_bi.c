94c693f5a3499c0ba4e8dc8f35d7dbbf35bb10a9
Mohammed Malih <mmalih@e2r4p3.1337.ma>
2023-02-17 05:53:29 +0100

    export norme

--- execution/export_bi.c
+++ execution/export_bi.c
@@ -6,7 +6,7 @@
 /*   By: mmalih <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 04:37:12 by mmalih            #+#    #+#             */
-/*   Updated: 2023/02/17 04:53:05 by mmalih           ###   ########.fr       */
+/*   Updated: 2023/02/17 05:41:59 by mmalih           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,17 +42,17 @@ int	export_builtin(char **args, t_envlist *env)
 		else if (ft_strchr(args[i], '=') != NULL)
 		{
 			tmp = get_key_value_pair(args[i]);
-            set_env_var(&env, tmp[0], tmp[1]);
-            free_str_tab(tmp);
-        }
-        i++;
-    }
-    return (ret);
+			set_env_var(&env, tmp[0], tmp[1]);
+			free_str_tab(tmp);
+		}
+		i++;
+	}
+	return (ret);
 }
 
-void    free_str_tab(char **str)
+void	free_str_tab(char **str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i] != NULL)
@@ -63,7 +63,59 @@ void    free_str_tab(char **str)
 	free(str);
 }
 
-int set_env_var(t_envlist **env, char *key, char *value)
+int	set_env_var(t_envlist **env, char *key, char *value)
+{
+	t_envlist	*temp;
+	t_envlist	*prev;
+
+	temp = *env;
+	prev = *env;
+	while (temp != NULL)
+	{
+		if (strcmp(temp->name, key) == 0)
+		{
+			free(temp->content);
+			temp->content = ft_strdup(value);
+			return (0);
+		}
+		prev = temp;
+		temp = temp->next;
+	}
+	temp = create_env_node(key, value);
+	if (*env == NULL)
+	{
+		*env = temp;
+		return (0);
+	}
+	prev->next = temp;
+	return (0);
+}
+
+t_envlist	*create_env_node(char *key, char *value)
+{
+	t_envlist	*node;
+
+	node = (t_envlist *) malloc(sizeof(t_envlist));
+	if (node == NULL)
+		return (NULL);
+	node->name = ft_strdup(key);
+	if (node->name == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->content = ft_strdup(value);
+	if (node->content == NULL)
+	{
+		free(node->name);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
+
+/*int set_env_var(t_envlist **env, char *key, char *value)
 {
     t_envlist   *temp;
     t_envlist   *prev;
@@ -92,80 +144,49 @@ int set_env_var(t_envlist **env, char *key, char *value)
     }
     prev->next = temp;
     return (0);
-}
-
-/*int set_env_var(t_envlist *env, char *key, char *value)
-{
-    t_envlist   temp;
-    t_envlist   prev;
-
-    temp = env;
-    prev = env;
-    while (temp->next != NULL)
-    {
-        if (ft_strcmp(temp->name, key) == 0)
-        {
-            free(temp->content);
-            temp->content = ft_strdup(value);
-            return (0);
-        }
-        prev = temp;
-        temp = temp->next;
-    }
-    if (ft_strcmp(temp->name, key) == 0)
-    {
-        free(temp->content);
-        temp->content = ft_strdup(value);
-        return (0);
-    }
-    temp->next = malloc(sizeof(t_envlist));
-    temp->next->name = ft_strdup(key);
-    temp->next->content = ft_strdup(value);
-    temp->next->next = NULL;
-    return (0);
 }*/
 
- char   **get_key_value_pair(char *str)
+char	**get_key_value_pair(char *str)
 {
-    char    **ret;
-    int     i;
-    int     j;
+	char	**ret;
+	int		i;
+	int		j;
 
-    ret = malloc(sizeof(char *) * 3);
-    ret[0] = malloc(sizeof(char) * (ft_strlen(str) + 1));
-    ret[1] = malloc(sizeof(char) * (ft_strlen(str) + 1));
-    ret[2] = NULL;
-    i = 0;
-    j = 0;
-    while (str[i] != '=')
-    {
-        ret[0][i] = str[i];
-        i++;
-    }
-    ret[0][i] = '\0';
-    i++;
-    while (str[i])
-    {
-        ret[1][j] = str[i];
-        i++;
-        j++;
-    }
-    ret[1][j] = '\0';
-    return (ret);
+	ret = malloc(sizeof(char *) * 3);
+	ret[0] = malloc(sizeof(char) * (ft_strlen(str) + 1));
+	ret[1] = malloc(sizeof(char) * (ft_strlen(str) + 1));
+	ret[2] = NULL;
+	i = 0;
+	j = 0;
+	while (str[i] != '=')
+	{
+		ret[0][i] = str[i];
+		i++;
+	}
+	ret[0][i] = '\0';
+	i++;
+	while (str[i])
+	{
+		ret[1][j] = str[i];
+		i++;
+		j++;
+	}
+	ret[1][j] = '\0';
+	return (ret);
 }
 
-int is_valid_env_var_key(char *str)
+int	is_valid_env_var_key(char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (ft_isdigit(str[i]))
-        return (0);
-    while (str[i])
-    {
-        if (!ft_isalnum(str[i]) && str[i] != '_')
-            return (0);
-        i++;
-    }
-    return (1);
+	i = 0;
+	if (ft_isdigit(str[i]))
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
 }
