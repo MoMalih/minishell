9716348370ec0704a1c740e9607c360ba1538428
Mohammed Malih <mmalih@e1r2p8.1337.ma>
2023-02-18 04:30:20 +0100

    unset norme

--- execution/unset_bi.c
+++ execution/unset_bi.c
@@ -6,13 +6,64 @@
 /*   By: mmalih <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 21:19:46 by mmalih            #+#    #+#             */
-/*   Updated: 2023/02/17 21:19:50 by mmalih           ###   ########.fr       */
+/*   Updated: 2023/02/18 04:29:28 by mmalih           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"sshell.h"
 
-int unset_builtin(char **args, t_envlist **env)
+int	unset_builtin(char **args, t_envlist **env)
+{
+	t_envlist	*found;
+
+	if (args[1] == NULL)
+	{
+		panic("unset: not enough arguments");
+		return (1);
+	}
+	found = find_env_variable(args[1], env);
+	if (found == NULL)
+	{
+		panic("unset: variable not found");
+		return (1);
+	}
+	remove_env_variable(found, env);
+	return (0);
+}
+
+t_envlist	*find_env_variable(char *name, t_envlist **env)
+{
+	t_envlist	*current;
+
+	current = env;
+	while (current != NULL)
+	{
+		if (ft_strcmp(current->name, name) == 0)
+			return (current);
+		current = current->next;
+	}
+	return (NULL);
+}
+
+void	remove_env_variable(t_envlist *var, t_envlist **env)
+{
+	t_envlist	*current;
+
+	if (var == *env)
+		*env = (*env)->next;
+	else
+	{
+		current = env;
+		while (current != NULL && current->next != var)
+			current = current->next;
+		if (current != NULL)
+			current->next = var->next;
+	}
+	free(var->name);
+	free(var);
+}
+
+/*int unset_builtin(char **args, t_envlist **env)
 {
     t_envlist   *temp;
     t_envlist   *prev;
@@ -48,7 +99,7 @@ int unset_builtin(char **args, t_envlist **env)
     return (1);
 }
 
-/*int unset_builtin(char **args, t_envlist *env)
+int unset_builtin(char **args, t_envlist *env)
 {
     t_envlist   temp;
     t_envlist   prev;
